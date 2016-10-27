6579840075c09cecbb429d7904aabf6e76a88d7d
张瑞 <zhangrui@Gmail.com>
2016-10-27 16:46:03 +0800

    我的fragment界面布局以及待实现的点击事件

---
+++ FoodPie/app/src/main/java/lanou/foodpie/fragment/LogInFragment.java
@@ -0,0 +1,24 @@
+package lanou.foodpie.fragment;
+
+import lanou.foodpie.R;
+import lanou.foodpie.abs.BaseFragment;
+
+/**
+ * Created by ZhangRui on 16/10/27.
+ */
+public class LogInFragment extends BaseFragment {
+    @Override
+    protected int getLayout() {
+        return R.layout.fragment_login;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
--- FoodPie/app/src/main/java/lanou/foodpie/fragment/MyFragment.java
+++ FoodPie/app/src/main/java/lanou/foodpie/fragment/MyFragment.java
@@ -5,6 +5,8 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 import lanou.foodpie.abs.BaseFragment;
 import lanou.foodpie.R;
@@ -13,12 +15,13 @@ import lanou.foodpie.R;
  * Created by ZhangRui on 16/10/21.
  * 我的Fragment
  */
-public class MyFragment extends BaseFragment {
-    @Nullable
-    @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(getLayout(),container,false);
-    }
+public class MyFragment extends BaseFragment implements View.OnClickListener {
+    private LinearLayout LlMyPhoto;
+    private LinearLayout LlMyCollection;
+    private LinearLayout LlFoodData;
+    private LinearLayout LlMyOrderLl;
+    private Button btnLogIn;
+
 
     @Override
     protected void initData() {
@@ -27,11 +30,39 @@ public class MyFragment extends BaseFragment {
 
     @Override
     protected void initView() {
+        LlMyPhoto = bindView(R.id.myPhotoLl);
+        LlMyCollection = bindView(R.id.myCollectionLl);
+        LlFoodData =bindView(R.id.foodDataLl);
+        LlMyOrderLl = bindView(R.id.myOrderLl);
+        btnLogIn = bindView(R.id.myLogInBtn);
 
+        LlMyPhoto.setOnClickListener(this);
+        LlMyCollection.setOnClickListener(this);
+        LlFoodData.setOnClickListener(this);
+        LlMyOrderLl.setOnClickListener(this);
+        btnLogIn.setOnClickListener(this);
     }
 
     @Override
     protected int getLayout() {
         return R.layout.fragment_my;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.myPhotoLl:
+
+                break;
+            case R.id.myCollectionLl:
+                break;
+            case R.id.foodDataLl:
+                break;
+            case R.id.myOrderLl:
+                break;
+            case R.id.myLogInBtn:
+                break;
+
+        }
     }
 }
